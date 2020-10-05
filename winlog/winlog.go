google/winops

commit 4a3524d1a285fa9a11b05c8d989a9cb533d0bcb3

Currently we detect the specific case that an event has no associated publisher metadata, and in that case return the event un-augmented. However in other cases such as OpenPublisherMetadata we error out of the whole routine.
It turns out that other buggy applications are easily able to poison the Event
Log in ways that cause OpenPublisherMetadata to fail. We've also seen other
failures that aren't obviously explained by this feature of the Event Log.

Either way, let's just return the data we _do_ have, so that one poisoned event
can't prevent us from reading subsequent events that show up in the
subscription.

PiperOrigin-RevId: 334855976

winlog/winlog.go
@@ -9,6 +9,7 @@ import (
 	"syscall"
 	"unsafe"
 
+	log "github.com/golang/glog"
 	"golang.org/x/sys/windows"
 	"github.com/google/winops/winlog/wevtapi"
 )
@@ -102,15 +103,16 @@ func GetRenderedEvents(config *SubscribeConfig, publisherCache map[string]window
 	var renderedEvents []string
 	for _, event := range events[:returned] {
 		// Render the basic XML representation of the event.
-		renderedEvent, err := RenderFragment(event, wevtapi.EvtRenderEventXml)
+		fragment, err := RenderFragment(event, wevtapi.EvtRenderEventXml)
 		if err != nil {
 			return nil, fmt.Errorf("RenderEventXML failed: %v", err)
 		}
 
 		// Attempt to render the full event using the basic event.
-		renderedEvent, err = RenderFormattedMessageXML(event, renderedEvent, locale, publisherCache)
+		renderedEvent, err := RenderFormattedMessageXML(event, fragment, locale, publisherCache)
 		if err != nil {
-			return nil, fmt.Errorf("RenderFormattedMessageXML failed: %v", err)
+			log.Errorf("Failed to fully render event, returning fragment: %v\n%v", err, fragment)
+			renderedEvent = fragment
 		}
 		renderedEvents = append(renderedEvents, renderedEvent)
 	}

commit 52e79c9083ace1513c017e59b64b706fb890924e

Don't bail out of GetRenderedEvents even if rendering fails entirely
In commit 4a3524d1a285 I changed GetRenderedEvents to log and soldier on when
it gets an error from RenderFormattedMessageXML. I was forgetting that we
_also_ experience errors from RenderFragment. We should treat them the same to
unblock these hosts that appear to have poisoned logs.

NB for RenderFormattedMessageXML failures, we print the fragment XML -
hopefully this will let us track down the source of the poisoning. We don't
have the same info for RenderFragment failures. I propose we go ahead anyway,
and fix the RenderFormattedMessageXML failures; we can then further investigate
the RenderFragment failures if they don't go away.

PiperOrigin-RevId: 335444819

winlog/winlog.go
@@ -105,7 +105,8 @@ func GetRenderedEvents(config *SubscribeConfig, publisherCache map[string]window
 		// Render the basic XML representation of the event.
 		fragment, err := RenderFragment(event, wevtapi.EvtRenderEventXml)
 		if err != nil {
-			return nil, fmt.Errorf("RenderEventXML failed: %v", err)
+			log.Errorf("Failed to render event with EvtRenderEventXml, skipping: %v", err)
+			continue
 		}
 
 		// Attempt to render the full event using the basic event.